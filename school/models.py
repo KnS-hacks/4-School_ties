4f8931ef0b0cc033cd0a5731e012dcd5ac8ee590
Jiyoung <thdwldud8@gmail.com>
2021-11-15 23:03:05 +0900

    [BE][지영] :: notice model생성 및 static setting

--- school/models.py
+++ school/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# notice CRUD
+class Notice(models.Model) :
+    title = models.CharField(max_length=2000)
+    author = models.CharField(max_length=50)
+    pub_date = models.DateField()
+    body = models.TextField()
+    notice_image = models.ImageField(upload_to = "notice/", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- school/views.py
+++ school/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def notice(request):
+    Notices = Notice.objects.all()
+    return render(request, 'notice.html',{'Notices':Notices})
