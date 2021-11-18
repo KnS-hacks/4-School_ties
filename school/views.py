866a64ccb1e37b56bba7e899418c22a8d8461e50
eunseoJeong <em4784@gmail.com>
2021-11-19 03:39:29 +0900

    [BE][은서] :: 랭킹 학부생만 + 작성자가 수정삭제하기. 해야해 ...

--- school/views.py
+++ school/views.py
@@ -2,21 +2,32 @@ from django.shortcuts import redirect, render, get_object_or_404
 from .models import *
 from django.utils import timezone
 from account.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 ### 공지사항
+@login_required
 def notice(request):
     Notices = Notice.objects.all()
     return render(request, 'notice.html',{'Notices':Notices})
 
 # 공지사항 create
+@login_required
+@staff_member_required
 def notice_post(request):
     if request.method == 'POST':
         new_notice = Notice()
+        user_id = request.user.id
+        # user_id 값과 User 모델의 객체 중 일치하는 값. 즉 글 작성자의 user 객체를 user 변수에 저장합니다. 
+        user = User.objects.get(id = user_id)
+        print(user)
+        # 작성자 = user 가 됩니다. 
         new_notice.Notice_title = request.POST['title']
-        new_notice.Notice_author = request.POST['author']
+        new_notice.Notice_author = user
         new_notice.Notice_image = request.FILES.get('image')
         new_notice.Notice_body = request.POST['body']
         new_notice.Notice_pub_date = timezone.now()
@@ -29,13 +40,16 @@ def notice_post(request):
     else:
         return render(request, 'notice_new.html')
 
-#공지사항 detail
+#공지사항 
+@login_required
 def notice_detail(request, id):
     detail_notice = get_object_or_404(Notice, pk = id)
     print(detail_notice)
     return render(request, 'notice_detail.html', {'detail_notice':detail_notice})
 
 #공지사항 update
+@login_required
+@staff_member_required
 def notice_edit(request, id):
     notice = Notice.objects.get(id = id)
     return render(request, 'notice_edit.html', {'notice': notice})
@@ -51,6 +65,8 @@ def notice_update(request, id):
     return redirect('notice_detail', notice_update.id)
 
 #공지사항 삭제
+@login_required
+@staff_member_required
 def notice_delete(request, id):
     notice_delete = Notice.objects.get(id = id)
     user_id = request.user.id
@@ -61,23 +77,30 @@ def notice_delete(request, id):
     return redirect('notice')
 
 #****공지사항 댓글*******
+@login_required
 def create_notice_comment(request, notice_id):
     if request.method == 'POST':
         notice_comment = Notice_Comment()
+        user_id = request.user.id
+        user = User.objects.get(id = user_id)
+        print(user)
+        # 작성자 = user 가 됩니다. 
         notice_comment.notice = get_object_or_404(Notice, pk=notice_id)
-        notice_comment.notice_author = request.POST['author']
+        notice_comment.notice_author = user
         notice_comment.notice_content = request.POST['content']
         notice_comment.notice_at = timezone.datetime.now()
         notice_comment.save()
         return redirect('notice_detail', notice_id)
 
 # 공지사항 댓글 delete
+@login_required
 def delete_notice_comment(request, id, comment_id):
     delete_notice_comment = Notice_Comment.objects.get(pk = comment_id)
     delete_notice_comment.delete()
     return redirect('notice_detail', id)
 
 # 공지사항 댓글 update
+@login_required
 def update_notice_comment(request, id, comment_id):
     if request.method == 'POST':
         update_notice_comment = get_object_or_404(Notice_Comment, pk=comment_id)
@@ -92,6 +115,7 @@ def update_notice_comment(request, id, comment_id):
 
 
 ###자유게시판
+@login_required
 def free_board(request):
     Free_boards = Free_board.objects.all()
     return render(request, 'free.html',{'Free_boards':Free_boards})
@@ -99,32 +123,40 @@ def free_board(request):
 
 
 ### 스터디 게시판 ??
+@login_required
+
 def study_board(request):
     Study_boards = Study_board.objects.all()
     return render(request, 'study.html',{'Study_boards':Study_boards})
 
 ### 공모전 게시판
+@login_required
+
 def contest_board(request):
     Contest_boards = Contest_board.objects.all()
     return render(request, 'contest.html',{'Contest_boards':Contest_boards})
 
 ### 졸업생 게시판
+@login_required
 def graduate_board(request):
     Graduate_boards = Graduate_board.objects.all()
     return render(request, 'graduate.html',{'Graduate_boards':Graduate_boards})
 
 ### 소모임 게시판
+@login_required
 def club_board(request):
     Club_boards = Club_board.objects.all()
     return render(request, 'club.html',{'Club_boards':Club_boards})
 
 # 중고 서적 게시판
+@login_required
 def market_board(request):
     Market_boards = Market_board.objects.all()
     return render(request, 'club.html',{'Market_boards':Market_boards})
 
 
 #게시판 랭킹
+@login_required
 def rank(request):
-    rank = User.objects.all().order_by('-rank_count')
+    rank = User.objects.filter(status = "학부생").order_by('-rank_count')
     return render(request, 'rank.html', {"rank":rank})
